c22bc25bf24ebbb6a62c469fe7c8d8cad83c9ec2
yskszk63 <yskszk63@gmail.com>
2021-12-25 13:05:40 +0900

    wip:marshall/unmarshall

---
+++ types.test.ts
@@ -0,0 +1,198 @@
+// TODO check overflow
+// TODO byte order
+import { assertEquals } from "https://deno.land/std@0.118.0/testing/asserts.ts";
+
+import * as t from './types.ts';
+
+function memStream(mem: Uint8Array[]): [ReadableStream<Uint8Array>, WritableStream<Uint8Array>] {
+  const r = new ReadableStream({
+    pull(controller) {
+      while (mem.length && mem[0].length === 0) {
+        mem.shift();
+      }
+
+      if (!mem.length) {
+        controller.close();
+
+      } else if (controller.byobRequest && controller.byobRequest.view) {
+        const view = controller.byobRequest.view;
+        const n = Math.min(view.byteLength, mem[0].byteLength);
+        const m = mem[0].subarray(0, n);
+        mem[0] = mem[0].subarray(n);
+        new Uint8Array(view.buffer).set(m);
+        controller.byobRequest.respond(n);
+
+      } else {
+        const chunk = mem.shift();
+        controller.enqueue(chunk!);
+      }
+    },
+    type: 'bytes',
+  });
+
+  const w = new WritableStream({
+    write(chunk) {
+      mem.push(chunk);
+    },
+  });
+
+  return [r, w];
+}
+
+
+Deno.test('byte', async () => {
+  const ty = t.byte();
+
+  for (const v of [0, 1, 192, 255]) {
+    const mem: Uint8Array[] = [];
+    const [r, w] = memStream(mem);
+
+    await ty.marshall(v, w.getWriter());
+    const unmarshalled = await ty.unmarshall(r.getReader({ mode: 'byob' }));
+    assertEquals(unmarshalled, v);
+  }
+});
+
+Deno.test('boolean', async () => {
+  const ty = t.boolean();
+
+  for (const v of [true, false]) {
+    const mem: Uint8Array[] = [];
+    const [r, w] = memStream(mem);
+
+    await ty.marshall(v, w.getWriter());
+    const unmarshalled = await ty.unmarshall(r.getReader({ mode: 'byob' }));
+    assertEquals(unmarshalled, v);
+  }
+});
+
+Deno.test('int16', async () => {
+  const ty = t.int16();
+
+  for (const v of [0, 1, 32, 512, -512, 0x7FFF]) {
+    const mem: Uint8Array[] = [];
+    const [r, w] = memStream(mem);
+
+    await ty.marshall(v, w.getWriter());
+    const unmarshalled = await ty.unmarshall(r.getReader({ mode: 'byob' }));
+    assertEquals(unmarshalled, v);
+  }
+});
+
+Deno.test('uint16', async () => {
+  const ty = t.uint16();
+
+  for (const v of [0, 1, 32, 512, 0x7FFF]) {
+    const mem: Uint8Array[] = [];
+    const [r, w] = memStream(mem);
+
+    await ty.marshall(v, w.getWriter());
+    const unmarshalled = await ty.unmarshall(r.getReader({ mode: 'byob' }));
+    assertEquals(unmarshalled, v);
+  }
+});
+
+Deno.test('int32', async () => {
+  const ty = t.int32();
+
+  for (const v of [0, 1, 32, 512, -512, 0x7FFF_FFFF]) {
+    const mem: Uint8Array[] = [];
+    const [r, w] = memStream(mem);
+
+    await ty.marshall(v, w.getWriter());
+    const unmarshalled = await ty.unmarshall(r.getReader({ mode: 'byob' }));
+    assertEquals(unmarshalled, v);
+  }
+});
+
+Deno.test('uint32', async () => {
+  const ty = t.uint32();
+
+  for (const v of [0, 1, 32, 512, 0xFFFF_FFFF]) {
+    const mem: Uint8Array[] = [];
+    const [r, w] = memStream(mem);
+
+    await ty.marshall(v, w.getWriter());
+    const unmarshalled = await ty.unmarshall(r.getReader({ mode: 'byob' }));
+    assertEquals(unmarshalled, v);
+  }
+});
+
+Deno.test('int64', async () => {
+  const ty = t.int64();
+
+  for (const v of [0n, 1n, 32n, 512n, -512n, 0x7FFF_FFFF_FFFF_FFFFn]) {
+    const mem: Uint8Array[] = [];
+    const [r, w] = memStream(mem);
+
+    await ty.marshall(v, w.getWriter());
+    const unmarshalled = await ty.unmarshall(r.getReader({ mode: 'byob' }));
+    assertEquals(unmarshalled, v);
+  }
+});
+
+Deno.test('uint64', async () => {
+  const ty = t.uint64();
+
+  for (const v of [0n, 1n, 32n, 512n, 0x7FFF_FFFF_FFFF_FFFFn]) {
+    const mem: Uint8Array[] = [];
+    const [r, w] = memStream(mem);
+
+    await ty.marshall(v, w.getWriter());
+    const unmarshalled = await ty.unmarshall(r.getReader({ mode: 'byob' }));
+    assertEquals(unmarshalled, v);
+  }
+});
+
+Deno.test('double', async () => {
+  const ty = t.double();
+
+  for (const v of [0.1, 0.3]) {
+    const mem: Uint8Array[] = [];
+    const [r, w] = memStream(mem);
+
+    await ty.marshall(v, w.getWriter());
+    const unmarshalled = await ty.unmarshall(r.getReader({ mode: 'byob' }));
+    assertEquals(unmarshalled, v);
+  }
+});
+
+Deno.test('string', async () => {
+  const ty = t.string();
+
+  for (const v of ["", "Hello, World!"]) {
+    const mem: Uint8Array[] = [];
+    const [r, w] = memStream(mem);
+
+    await ty.marshall(v, w.getWriter());
+    const unmarshalled = await ty.unmarshall(r.getReader({ mode: 'byob' }));
+    assertEquals(unmarshalled, v);
+  }
+});
+
+Deno.test('object_path', async () => {
+  const ty = t.objectPath();
+
+  for (const v of ["/obj/path"]) {
+    const mem: Uint8Array[] = [];
+    const [r, w] = memStream(mem);
+
+    await ty.marshall(v, w.getWriter());
+    const unmarshalled = await ty.unmarshall(r.getReader({ mode: 'byob' }));
+    assertEquals(unmarshalled, v);
+  }
+});
+
+Deno.test('signature', async () => {
+  const ty = t.signature();
+
+  for (const v of ["i"]) {
+    const mem: Uint8Array[] = [];
+    const [r, w] = memStream(mem);
+
+    await ty.marshall(v, w.getWriter());
+    const unmarshalled = await ty.unmarshall(r.getReader({ mode: 'byob' }));
+    assertEquals(unmarshalled, v);
+  }
+});
+
--- types.ts
+++ types.ts
@@ -1,41 +1,175 @@
+async function marshallFixed<T>(typedarray: { of: (b: T) => ArrayBufferView } , val: T, out: WritableStreamDefaultWriter<Uint8Array>): Promise<number> {
+  const buf = typedarray.of(val);
+  await out.write(new Uint8Array(buf.buffer));
+  return buf.byteLength;
+}
+
+async function unmarshallFixed<R>(typedarray: (new (b: ArrayBuffer) => ArrayBufferView & ArrayLike<R>) & { BYTES_PER_ELEMENT: number }, input: ReadableStreamBYOBReader): Promise<R> {
+  const result = await input.read(new Uint8Array(new ArrayBuffer(typedarray.BYTES_PER_ELEMENT)));
+  if (result.done) {
+    throw new Error('unexpected EOF.');
+  }
+  const buf = new typedarray(result.value.buffer);
+  if (!buf.length) {
+    throw new Error();
+  }
+  return buf[0];
+}
+
+async function marshallText(alignment: 1 | 4, text: string, out: WritableStreamDefaultWriter<Uint8Array>): Promise<number> {
+  const buf = new TextEncoder().encode(text);
+  const len = buf.byteLength;
+  const pad = (alignment - ((len + 1) % alignment) % alignment);
+
+  const n = await marshallFixed(Uint32Array, len, out);
+  out.write(buf);
+  out.write(new Uint8Array(1 + pad));
+
+  return n + len + 1 + pad;
+}
+
+async function unmarshallText(alignment: 1 | 4, input: ReadableStreamBYOBReader): Promise<string> {
+  const len = await unmarshallFixed(Uint32Array, input);
+  const pad = (alignment - ((len + 1) % alignment) % alignment);
+  const v = new Uint8Array(len + 1 + pad);
+  const result = await input.read(v);
+  if (result.done) {
+    throw new Error('unexpected EOF.');
+  }
+  return new TextDecoder().decode(result.value.subarray(0, len));
+}
+
+
 abstract class DbusType<Output> {
   readonly _output!: Output;
+
+  marshall(_val: Output, _out: WritableStreamDefaultWriter<Uint8Array>): Promise<number> {
+    throw new Error('not implemented');
+  }
+
+  unmarshall(_input: ReadableStreamBYOBReader): Promise<Output> {
+    throw new Error('not implemented');
+  }
 }
 
 class DbusByte extends DbusType<number> {
+  marshall(val: number, out: WritableStreamDefaultWriter<Uint8Array>): Promise<number> {
+    return marshallFixed(Uint8Array, val, out);
+  }
+
+  unmarshall(input: ReadableStreamBYOBReader): Promise<number> {
+    return unmarshallFixed(Uint8Array, input);
+  }
 }
 
 class DbusBoolean extends DbusType<boolean> {
+  marshall(val: boolean, out: WritableStreamDefaultWriter<Uint8Array>): Promise<number> {
+    return marshallFixed(Uint8Array, val ? 1 : 0, out);
+  }
+
+  async unmarshall(input: ReadableStreamBYOBReader): Promise<boolean> {
+    return unmarshallFixed(Uint8Array, input).then(v => v === 1);
+  }
 }
 
 class DbusInt16 extends DbusType<number> {
+  marshall(val: number, out: WritableStreamDefaultWriter<Uint8Array>): Promise<number> {
+    return marshallFixed(Int16Array, val, out);
+  }
+
+  unmarshall(input: ReadableStreamBYOBReader): Promise<number> {
+    return unmarshallFixed(Int16Array, input);
+  }
 }
 
 class DbusUint16 extends DbusType<number> {
+  marshall(val: number, out: WritableStreamDefaultWriter<Uint8Array>): Promise<number> {
+    return marshallFixed(Uint16Array, val, out);
+  }
+
+  unmarshall(input: ReadableStreamBYOBReader): Promise<number> {
+    return unmarshallFixed(Uint16Array, input);
+  }
 }
 
 class DbusInt32 extends DbusType<number> {
+  marshall(val: number, out: WritableStreamDefaultWriter<Uint8Array>): Promise<number> {
+    return marshallFixed(Int32Array, val, out);
+  }
+
+  unmarshall(input: ReadableStreamBYOBReader): Promise<number> {
+    return unmarshallFixed(Int32Array, input);
+  }
 }
 
 class DbusUint32 extends DbusType<number> {
+  marshall(val: number, out: WritableStreamDefaultWriter<Uint8Array>): Promise<number> {
+    return marshallFixed(Uint32Array, val, out);
+  }
+
+  unmarshall(input: ReadableStreamBYOBReader): Promise<number> {
+    return unmarshallFixed(Uint32Array, input);
+  }
 }
 
 class DbusInt64 extends DbusType<bigint> {
+  marshall(val: bigint, out: WritableStreamDefaultWriter<Uint8Array>): Promise<number> {
+    return marshallFixed(BigInt64Array, val, out);
+  }
+
+  unmarshall(input: ReadableStreamBYOBReader): Promise<bigint> {
+    return unmarshallFixed(BigInt64Array, input);
+  }
 }
 
 class DbusUint64 extends DbusType<bigint> {
+  marshall(val: bigint, out: WritableStreamDefaultWriter<Uint8Array>): Promise<number> {
+    return marshallFixed(BigUint64Array, val, out);
+  }
+
+  unmarshall(input: ReadableStreamBYOBReader): Promise<bigint> {
+    return unmarshallFixed(BigUint64Array, input);
+  }
 }
 
 class DbusDouble extends DbusType<number> {
+  marshall(val: number, out: WritableStreamDefaultWriter<Uint8Array>): Promise<number> {
+    return marshallFixed(Float64Array, val, out);
+  }
+
+  unmarshall(input: ReadableStreamBYOBReader): Promise<number> {
+    return unmarshallFixed(Float64Array, input);
+  }
 }
 
 class DbusString extends DbusType<string> {
+  marshall(val: string, out: WritableStreamDefaultWriter<Uint8Array>): Promise<number> {
+    return marshallText(4, val, out);
+  }
+
+  unmarshall(input: ReadableStreamBYOBReader): Promise<string> {
+    return unmarshallText(4, input);
+  }
 }
 
 class DbusObjectPath extends DbusType<string> {
+  marshall(val: string, out: WritableStreamDefaultWriter<Uint8Array>): Promise<number> {
+    return marshallText(4, val, out);
+  }
+
+  unmarshall(input: ReadableStreamBYOBReader): Promise<string> {
+    return unmarshallText(4, input);
+  }
 }
 
 class DbusSignature extends DbusType<string> {
+  marshall(val: string, out: WritableStreamDefaultWriter<Uint8Array>): Promise<number> {
+    return marshallText(1, val, out);
+  }
+
+  unmarshall(input: ReadableStreamBYOBReader): Promise<string> {
+    return unmarshallText(1, input);
+  }
 }
 
 class DbusArray<T> extends DbusType<T[]> {
@@ -72,86 +206,87 @@ class DbusUnixFd extends DbusType<number> {
 }
 
 type TypeOf<T extends DbusType<any>> = T['_output'];
+export type { TypeOf as infer }
 
 const _byte = new DbusByte();
-function byte(): DbusByte {
+export function byte(): DbusByte {
   return _byte;
 }
 
 const _boolean = new DbusBoolean();
-function boolean(): DbusBoolean {
+export function boolean(): DbusBoolean {
   return _boolean;
 }
 
 const _int16 = new DbusInt16();
-function int16(): DbusInt16 {
+export function int16(): DbusInt16 {
   return _int16;
 }
 
 const _uint16 = new DbusUint16();
-function uint16(): DbusUint16 {
+export function uint16(): DbusUint16 {
   return _uint16;
 }
 
 const _int32 = new DbusInt32();
-function int32(): DbusInt32 {
+export function int32(): DbusInt32 {
   return _int32;
 }
 
 const _uint32 = new DbusUint32();
-function uint32(): DbusUint32 {
+export function uint32(): DbusUint32 {
   return _uint32;
 }
 
 const _int64 = new DbusInt64();
-function int64(): DbusInt64 {
+export function int64(): DbusInt64 {
   return _int64;
 }
 
 const _uint64 = new DbusUint64();
-function uint64(): DbusUint64 {
+export function uint64(): DbusUint64 {
   return _uint64;
 }
 
 const _double = new DbusDouble();
-function double(): DbusDouble {
+export function double(): DbusDouble {
   return _double;
 }
 
 const _string = new DbusString();
-function string(): DbusString {
+export function string(): DbusString {
   return _string;
 }
 
 const _objectPath = new DbusObjectPath();
-function objectPath(): DbusObjectPath {
+export function objectPath(): DbusObjectPath {
   return _objectPath;
 }
 
 const _signature = new DbusSignature();
-function signature(): DbusSignature {
+export function signature(): DbusSignature {
   return _signature;
 }
 
-function array<T>(elementType: DbusType<T>): DbusArray<T> {
+export function array<T>(elementType: DbusType<T>): DbusArray<T> {
   return new DbusArray(elementType);
 }
 
-function struct<T extends [any, ...any[]]>(items: { [P in keyof T]: DbusType<T[P]> }): DbusStruct<T> {
+export function struct<T extends [any, ...any[]]>(items: { [P in keyof T]: DbusType<T[P]> }): DbusStruct<T> {
   return new DbusStruct<T>(items);
 }
 
 const _variant = new DbusVariant();
-function variant(): DbusVariant {
+export function variant(): DbusVariant {
   return _variant;
 }
 
-function dictEntry<K, V>(key: DbusType<K>, val: DbusType<V>): DbusDictEntry<K, V> {
+export function dictEntry<K, V>(key: DbusType<K>, val: DbusType<V>): DbusDictEntry<K, V> {
   return new DbusDictEntry(key, val);
 }
 
 const _unixFd = new DbusUnixFd();
-function unixFd(): DbusUnixFd {
+export function unixFd(): DbusUnixFd {
   return _unixFd;
 }
 
